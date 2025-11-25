a69bd08fc8fc3a4ac1d23051659cfc9dad511a45
victor <victorfmarques12@gmail.com>
2025-11-25 19:26:25 +0000

    .

--- ordenação/binaria.cpp
+++ ordenação/binaria.cpp
@@ -2,34 +2,66 @@
 
 using namespace std;
 
-int buscaBinaria(int vetor[], int tamanho, int valor){
-    int inicio = 0; 
+int buscaBinaria(int vetor[], int tamanho, int valor) {
+    int inicio = 0;
     int fim = tamanho - 1;
-    int meio = 0;
 
     while (inicio <= fim) {
-        int meio = (inicio + fim)/2;
+        int meio = inicio + (fim - inicio) / 2;
 
-        if(vetor[meio] == valor){
-            return meio;
-        } else if (vetor[meio] < valor){
+        if (vetor[meio] == valor) {
+            return meio; // Retorna a posicao do valor encontrado
+        } else if (vetor[meio] < valor) {
             inicio = meio + 1;
-        } else{
+        } else {
             fim = meio - 1;
         }
     }
-    
-    return -1;
+
+    return -1; // Retorna -1 se o valor nao for encontrado
 }
 
-int main() {
-    int vetor[] = {3, 6, 9, 12, 15, 18, 21};
-    int valor = 15;
-    int pos = buscaBinaria(vetor, 7, valor);
+int buscaBinariaComp(int vetor[], int tamanho, int valor) {
+    int inicio = 0;
+    int fim = tamanho - 1;
+    int count = 0;
 
-    if (pos != -1)
-        cout << "Valor: " << valor << " encontrado na posicao: " << pos << ".\n";
-    else
-        cout << "Valor nao encontrado.\n";
-    return 0;
+    while (inicio <= fim) {
+        int meio = inicio + (fim - inicio) / 2;
+        count++;
+
+        if (vetor[meio] == valor) {
+            return count; 
+        } else if (vetor[meio] < valor) {
+            inicio = meio + 1;
+        } else {
+            fim = meio - 1;
+        }
+    }
+
+    return -1; // Retorna -1 se o valor nao for encontrado
+}
+
+int main(){
+    cout << "Questão 1: \n";
+    int vetor[] = {2, 4, 6, 8, 10, 12, 13, 14, 16, 18};
+    int valor = 13;
+    int pos = buscaBinaria(vetor, 10, valor);
+
+    if(pos != -1){
+        cout << "Valor: " << valor << " encontrado na posição: " << pos << endl;
+    } else {
+        cout << "Valor não encontrado.";
+    }
+
+    cout << "Questão 2: \n";
+    int vetor2[] = {10, 20,30 ,40 ,50 ,60 ,70, 80, 90, 100};
+    int valor2 = 100;
+    int comp = buscaBinariaComp(vetor, 10, valor);
+
+    if(comp != -1){
+        cout << "Valor: " << valor2 << " encontrado com " << comp << " comparações." << endl;
+    } else {
+        cout << "Valor não encontrado.";
+    }
 }
--- ordenação/sequencial.cpp
+++ ordenação/sequencial.cpp
@@ -8,16 +8,56 @@ int buscaSequencial(int vetor[], int tamanho, int valor) {
         return i; // retorna índice se encontrado
     }
     return -1; // não encontrado
-    }   
+}
+int buscaSequencialContador(int vetor[], int tamanho, int valor) {
+    int contador = 0;
+    for (int i = 0; i < tamanho; i++) {
+        if (vetor[i] == valor) {
+            cout << "Valor: " << valor << " encontrado na posicao: " << i << ".\n";
+            contador++;
+        }
+    }
+    
+    if(contador == 0) {
+        return -1; // Valor não encontrado
+    } else {
+        return contador; // Retorna o número de ocorrências encontradas
+    }
+}
 
 int main() {
-    int vetor[] = {4, 8, 15, 16, 23, 42};
-    int valor = 15;
-    int pos = buscaSequencial(vetor, 6, valor);
+    // Questão 1:
+    cout << "Questão 1: \n";
+    int vetor[] = {15, 8, 25, 7, 12, 18, 5, 9, 30, 3};
+    int valor = 25;
+    int pos = buscaSequencial(vetor, 10, valor);
 
     if (pos != -1)
     cout <<"Valor: " << valor << " encontrado na posicao: " << pos << ".\n";
     else
     cout << " Valor nao encontrado.\n";
-    return 0;
+
+    // Questão 2:
+    cout << "\nQuestão 2: \n";
+    int vetor2[] = {7, 3, 5, 7, 9, 2, 7, 1, 4, 6};
+    int valor2 = 7;
+    int quant = buscaSequencialContador(vetor2, 10, valor2);
+
+    if(quant == -1) {
+        cout << "Valor nao encontrado.\n";
+    } else {
+        cout << "Total de ocorrencias do valor " << valor2 << ": " << quant << ".\n";
+    }
+
+    // Questão 3
+    cout << "\nQuestão 3: \n";
+    int vetor3[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int valor3 = 55;
+    int pos3 = buscaSequencial(vetor3, 10, valor3);
+
+    if(pos3 == -1){
+        cout << "Valor: " << valor3 << " não encontrado.";
+    } else {
+        cout << "Valor: " << valor3 << " encotrado na posição: " << pos3 << endl;
+    }
 }
